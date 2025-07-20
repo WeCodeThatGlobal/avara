f1b97d8ff840bc3183ad444e2970a0fefc9a1840
Mukund Solanki <mukund@wecodethat.com>
2025-07-21 02:04:37 +0530

    refactor: Simplify MainLayout by removing unused components and integrating Navbar into RootLayout for improved structure

--- avara-storefront/src/app/layout.tsx
+++ avara-storefront/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { getBaseURL } from "@lib/util/env"
 import { Metadata } from "next"
 import "styles/globals.css"
+import Navbar from "../common/components/navbar/Navbar"
 
 export const metadata: Metadata = {
   metadataBase: new URL(getBaseURL()),
@@ -10,6 +11,7 @@ export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" data-mode="light">
       <body>
+        <Navbar />
         <main className="relative">{props.children}</main>
       </body>
     </html>
