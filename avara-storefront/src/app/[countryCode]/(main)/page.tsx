542ce7200b1f489325dcfb8b276d65625db2b90c
mukund-wecodethat <mukund@wecodethat.com>
2025-07-22 11:23:40 +0530

    feat: Add Banner Section UI (#3)

--- avara-storefront/src/app/[countryCode]/(main)/page.tsx
+++ avara-storefront/src/app/[countryCode]/(main)/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next"
 
 import FeaturedProducts from "@modules/home/components/featured-products"
 import Hero from "@modules/home/components/hero"
+import BannerSection from "@modules/home/components/banner-section"
 import { listCollections } from "@lib/data/collections"
 import { getRegion } from "@lib/data/regions"
 
@@ -30,7 +31,9 @@ export default async function Home(props: {
 
   return (
     <>
+     
       <Hero />
+      <BannerSection />
       <div className="py-12">
         <ul className="flex flex-col gap-x-6">
           <FeaturedProducts collections={collections} region={region} />
